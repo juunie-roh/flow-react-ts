ca83740a303f9f835ef778e1f0cee378f8411971
Juun <blacklily7513@gmail.com>
2023-07-20 22:07:08 +0900

    Task update

--- src/routes/Project.tsx
+++ src/routes/Project.tsx
@@ -3,10 +3,43 @@ import { ProductMainBanner, ProductBusinessBanner } from '../containers/sections
 import Article from '../containers/articles/Article'
 
 import '../styles/common.css'
+import { ArticleProps } from '../types/props'
 
 const Project = () => {
 
   const target = 'project';
+  const articles: Array<ArticleProps> = [
+    {
+      title: ["쉽고 강력한 히스토리 관리", null],
+      sub_title: "친숙한 SNS 피드 형태의 게시물로 업무를 공유하고 히스토리를 관리합니다.",
+      target: target,
+      number: 1
+    },
+    {
+      title: ["스마트한 파일 관리", "데스크탑과 모바일의 동기화"],
+      sub_title: "데스크탑과 모바일의 파일 동기화로 프로젝트 별 파일과 자료를 무기한 보관하고 신속히 검색해 찾아볼 수 있습니다.",
+      target: target,
+      number: 2
+    },
+    {
+      title: ["일정 공유 스마트 캘린더", null],
+      sub_title: "위치기반의 구글맵 & 플로우 캘린더와 연동되어 회의・미팅・출장 등 일정을 쉽고 빠르게 공유할 수 있습니다.",
+      target: target,
+      number: 3
+    },
+    {
+      title: ["외부인 및 협력사 초대", null],
+      sub_title: "카OO톡・네이O온・메일... 등 흩어져 있는 외부 협력사와 유기적인 소통이 가능합니다.",
+      target: target,
+      number: 4
+    },
+    {
+      title: ["관리자 · 참여자 권한 구분", null],
+      sub_title: "프로젝트 관리자와 일반 참여자간의 작성 및 조회 권한을 차등화하여 보안을 강화할 수 있습니다.",
+      target: target,
+      number: 5
+    }
+  ]
 
   return (
     <main>
@@ -15,36 +48,16 @@ const Project = () => {
                          target={target}
       />
 
-      <Article title={["쉽고 강력한 히스토리 관리", null]}
-               sub_title="친숙한 SNS 피드 형태의 게시물로 업무를 공유하고 히스토리를 관리합니다."
-               target={target}
-               number={1}
-      />
-
-      <Article title={["스마트한 파일 관리", "데스크탑과 모바일의 동기화"]}
-               sub_title="데스크탑과 모바일의 파일 동기화로 프로젝트 별 파일과 자료를 무기한
-                          보관하고 신속히 검색해 찾아볼 수 있습니다."
-               target={target}
-               number={2}
-      />
-
-      <Article title={["일정 공유 스마트 캘린더", null]}
-               sub_title="위치기반의 구글맵 & 플로우 캘린더와 연동되어 회의・미팅・출장 등 일정을 쉽고 빠르게 공유할 수 있습니다."
-               target={target}
-               number={3}
-      />
-      
-      <Article title={["외부인 및 협력사 초대", null]}
-               sub_title="카OO톡・네이O온・메일... 등 흩어져 있는 외부 협력사와 유기적인 소통이 가능합니다."
-               target={target}
-               number={4}
-      />
-      
-      <Article title={["관리자 · 참여자 권한 구분", null]}
-               sub_title="프로젝트 관리자와 일반 참여자간의 작성 및 조회 권한을 차등화하여 보안을 강화할 수 있습니다."
-               target={target}
-               number={5}
-      />
+      {
+        articles.map((article) => (
+          <Article key={target + article.number}
+                   title={article.title}
+                   sub_title={article.sub_title}
+                   target={article.target}
+                   number={article.number}
+          />
+        ))
+      }
       
       <ProductBusinessBanner />
     </main>
--- src/routes/Task.tsx
+++ src/routes/Task.tsx
@@ -1,11 +1,45 @@
 import React from 'react'
 import { ProductBusinessBanner, ProductMainBanner } from '../containers/sections'
+import Article from '../containers/articles/Article';
+import { ArticleProps } from '../types/props';
 
 import '../styles/common.css'
 
 const Task = () => {
 
   const target = 'task';
+  const articles: Array<ArticleProps> = [
+    {
+      title: ["업무 리스트 자동화 스마트한 필터링", null],
+      sub_title: '플로우에 등록된 모든 업무는 자동으로 리스트화되며, 전사의 업무를 한눈에 모니터링할 수 있습니다. 원하는 업무 구분 조건에 따라 빠르게 원하는 업무를 찾을 수 있습니다.',
+      target: target,
+      number: 1
+    },
+    {
+      title: ["명확한 업무 요청", null],
+      sub_title: '유용한 옵션을 지정해 명확한 업무 요청을 할 수 있습니다. 개인의 기억력에 의존하여 업무를 놓치지 마세요.',
+      target: target,
+      number: 2
+    },
+    {
+      title: ["할 일 (To do) 리스트", null],
+      sub_title: '간단한 업무는 할 일(To do) 리스트로 아주 쉽고 편리하게 관리할 수 있습니다.',
+      target: target,
+      number: 3
+    },
+    {
+      title: ["실시간 업무 알림과", "다양한 알림 옵션"],
+      sub_title: '동일 프로젝트 구성원들의 업무 진행 상태가 실시간 알림으로 전달되어 업무 공유가 편리해집니다.',
+      target: target,
+      number: 4
+    },
+    {
+      title: ["간트차트(WBS)", null],
+      sub_title: '직관적인 차트와 그리드 형태를 기반으로 프로젝트 계획, 실행을 유연하게 관리할 수 있습니다.',
+      target: target,
+      number: 5
+    }
+  ]
 
   return (
 
@@ -14,6 +48,17 @@ const Task = () => {
                          sub_title={["업무 시작부터 종료까지 5단계 상태로 구분하여 직관적인 워크", "플로우 시스템을 제공합니다."]}
                          target={target}
       />
+
+      {
+        articles.map((article) => (
+          <Article key={target + article.number}
+                   title={article.title}
+                   sub_title={article.sub_title}
+                   target={article.target}
+                   number={article.number}
+          />
+        ))
+      }
 
       <ProductBusinessBanner />
     </main>
