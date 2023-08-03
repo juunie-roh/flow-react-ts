de79ff2af28ba403603dbe9a3bb9ddaf2a4a530b
Juun <blacklily7513@gmail.com>
2023-08-03 17:15:24 +0900

    types fixed

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,3 +1,9 @@
+type TOGGLE_LOGIN = 'TOGGLE_LOGIN';
+type SET_NAME = 'SET_NAME';
+type SET_ID = 'SET_ID';
+type SET_INFO = 'SET_INFO';
+type SET_AUTH = 'SET_AUTH';
+
 type UserState = {
   isLoggedIn: boolean,
   userName: string,
@@ -7,13 +13,18 @@ type UserState = {
 }
 
 type UserAction =
-  | { type: 'TOGGLE_LOGIN'; isLoggedIn: boolean }
-  | { type: 'SET_NAME'; userName: string }
-  | { type: 'SET_ID'; userId: string }
-  | { type: 'SET_INFO'; userInfo: string }
-  | { type: 'SET_AUTH'; authorization: string };
+  | { type: TOGGLE_LOGIN; isLoggedIn: boolean }
+  | { type: SET_NAME; userName: string }
+  | { type: SET_ID; userId: string }
+  | { type: SET_INFO; userInfo: string }
+  | { type: SET_AUTH; authorization: string };
 
 export type {
+  TOGGLE_LOGIN,
+  SET_NAME,
+  SET_ID,
+  SET_INFO,
+  SET_AUTH,
   UserState,
   UserAction
 }
